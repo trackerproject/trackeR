ed73103863569321c6a0187f1f77fb75255b98c2
hfrick <h.frick@ucl.ac.uk>
2015-11-17 11:32:06 +0000

    data for readme and ATI workshop as rda

--- data-raw/run.R
+++ data-raw/run.R
@@ -15,3 +15,22 @@ run <- readContainer(filepath,
                      m = 11,
                      mc.cores = getOption("mc.cores", 2L))
 save(run, file = "data/run.rda")
+
+## ATI = all of June + 1st of July = 34 files, 27 sessions
+filedir <- system.file("extdata", package = "trackeR")
+runs <- readDirectory(filedir,
+                     aggregate = TRUE,
+                     table = "gps_data",
+                     timezone = "",
+                     sessionThreshold = 2,
+                     country = NULL,
+                     mask = TRUE,
+                     fromDistances = NULL,
+                     speedunit = list(tcx = "m_per_s", db3 = "km_per_h"),
+                     distanceunit = list(tcx = "m", db3 = "km"),
+                     cycling = FALSE,
+                     lgap = 30, lskip = 5, m = 11,
+                     mc.cores = getOption("mc.cores", 2L),
+                     verbose = TRUE)
+save(runs, file = "data/runs.rda")
+
