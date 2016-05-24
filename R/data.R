05c0a07459faee25276e61dc8e2ee2d92eed65eb
hfrick <h.frick@ucl.ac.uk>
2016-05-24 23:43:42 +0100

    updated documentation for "runs" data object

--- R/data.R
+++ R/data.R
@@ -5,5 +5,5 @@
 
 #' Training sessions.
 #'
-#' @format A \code{\link{trackeRdata}} object containing 26 running training sessions.
+#' @format A \code{\link{trackeRdata}} object containing 27 running training sessions.
 "runs"
