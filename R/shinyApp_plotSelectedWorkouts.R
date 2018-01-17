6b040da7ea4d4996fb38f7be8385b0e79452b930
Ioannis Kosmidis <i.kosmidis@ucl.ac.uk>
2018-01-17 17:27:30 +0000

    Improvements in plot_SelectedWorkouts (all plots, yaxes, and other)

--- R/shinyApp_plotSelectedWorkouts.R
+++ R/shinyApp_plotSelectedWorkouts.R
@@ -6,146 +6,130 @@
 #' @param var_units A character of the unit of measurement for the given variable (e.g. "[bmp]") generated using \code{lab_sum()}.
 #' @param var_name_units A character of the named unit of measurement for the given variable (e.g. "Heart Rate \n [bpm]") generated using \code{lab_sum()}.
 
-
 plot_selectedWorkouts <- function(x, session, what, var_units, var_name_units){
-  #plot(data, session = session, what = what)
-  threshold = TRUE
-  smooth = FALSE
-  trend = TRUE
-  dates = TRUE
-  #x <- runs
-  #session <- c(1:27)
-  #what <-  c("pace", "heart.rate", 'altitude')
+    threshold <- TRUE
+    smooth <- FALSE
+    dates <- TRUE
 
-  Series <- NULL
-
-  ## code inspired by autoplot.zoo
     if (is.null(session)) {
         session <- seq_along(x)
     }
     units <- getUnits(x)
-
     x <- x[session]
 
-
-
-  ## threshold
-  if (threshold){
-    dots <- list()
-    if (all(c("variable", "lower", "upper") %in% names(dots))){
-      ## thresholds provided by user
-      th <- data.frame(variable = dots$variable, lower = dots$lower, upper = dots$upper)
+    ## threshold
+    if (threshold){
+        dots <- list()
+        if (all(c("variable", "lower", "upper") %in% names(dots))){
+            ## thresholds provided by user
+            th <- data.frame(variable = dots$variable, lower = dots$lower, upper = dots$upper)
+        }
+        else {
+            ## default thresholds
+            cycling <- units$unit[units$variable == "cadence"] == "rev_per_min"
+            th <- generateDefaultThresholds(cycling)
+            ## th <- th[which(th$variable %in% what),]
+            ## w <- which(units$variable %in% what)
+            th <- changeUnits(th, variable = units$variable, unit = units$unit)
+        }
+        ## apply thresholds
+        x <- threshold(x, th)
     }
-    else {
-      ## default thresholds
-      cycling <- units$unit[units$variable == "cadence"] == "rev_per_min"
-      th <- generateDefaultThresholds(cycling)
-      ## th <- th[which(th$variable %in% what),]
-      ## w <- which(units$variable %in% what)
-      th <- changeUnits(th, variable = units$variable, unit = units$unit)
+
+    ## for plotting pace, always apply a threshold
+    ## upper threshold is based on preferred walking speed of 1.4 m/s,
+    ## see https://en.wikipedia.org/wiki/Preferred_walking_speed
+    if ("pace" %in% what){
+        conversionPace <- match.fun(paste("s_per_m", units$unit[units$variable == "pace"], sep = "2"))
+        thPace <- conversionPace(1 / 1.4)
+        x <- threshold(x, variable = "pace", lower = 0, upper = thPace)
     }
-    ## apply thresholds
-    x <- threshold(x, th)
-  }
 
-
-  ## for plotting pace, always apply a threshold
-  ## upper threshold is based on preferred walking speed of 1.4 m/s,
-  ## see https://en.wikipedia.org/wiki/Preferred_walking_speed
-  if ("pace" %in% what){
-    conversionPace <- match.fun(paste("s_per_m", units$unit[units$variable == "pace"], sep = "2"))
-    thPace <- conversionPace(1 / 1.4)
-    x <- threshold(x, variable = "pace", lower = 0, upper = thPace)
-  }
-
-  ## smooth
-  if (smooth) {
-    xo <- x
-    if (is.null(getOperations(x)$smooth)) {
-      x <- smoother(x, what = what, ...)
+    if (smooth) {
+        x <- smoother(x, what = what, ...)
     }
-    else {
-      warning("This object has already been smoothed. No additional smoothing takes place.")
-      smooth <- FALSE ## it's not the plot function calling smoother
-      x <- x
-    }
-  }
-  else {
-    x <- x
-  }
 
-  ## get data
-  df <- if (smooth) fortify(xo, melt = TRUE) else fortify(x, melt = TRUE)
+    df <- fortify(x, melt = TRUE)
 
     df$id <- session[df$SessionID]
-  ## prepare session id for panel header
-  if (dates) {
-    df$SessionID <- format(df$id)
-    df$SessionID <- gsub(" ", "0", df$SessionID)
-    df$SessionID <- paste(df$SessionID, format(df$Index, "%Y-%m-%d"), sep = ": ")
-  }
-  else {
-    df$SessionID <- factor(df$SessionID, levels = seq_along(session), labels = session)
-  }
-  df <- subset(df, Series %in% what)
-  ## df$Series <- factor(df$Series)
+    ## prepare session id for panel header
+    if (dates) {
+        df$SessionID <- format(df$id)
+        df$SessionID <- gsub(" ", "0", df$SessionID)
+        df$SessionID <- paste(df$SessionID, format(df$Index, "%Y-%m-%d"), sep = ": ")
+    }
+    else {
+        df$SessionID <- factor(df$SessionID, levels = seq_along(session), labels = session)
+    }
+    df <- subset(df, Series %in% what)
 
-  ## check that there is data to plot
-  ## for (l in levels(df$Series)) {
-  ##     if (all(is.na(subset(df, Series == l, select = "Value"))))
-  ##         df <- df[!(df$Series == l), ]
-  ## }
+    df$Series <- as.character(df$Series)
+    df$numericDate <- as.numeric(df$Index)
+    N <- length(unique(df$id))
 
-  #df$Index1 <- as.Date(df$Index, format = "%Y-%m-%d  %H:%M:%S")
-
-  #format(dataSelected$sessionStart, format = "%Y-%m-%d  %H:%M:%S")
-  df$Series <- as.character(df$Series)
-  df$numericDate <- as.numeric(df$Index)
-  N <- length(unique(df$id))
-
-  plot_stored <- list()
+    ranges <- NULL
     for (i in unique(df$id)) {
+        df_subset <- df[df$id == i,]
+        values <- df_subset[df_subset$Series == what, "Value"]
+        has_values <- !all(is.na(values))
+        current_range <- range(values, na.rm = TRUE)
+        if (any(is.infinite(current_range))) {
+            current_range <- c(NA, NA)
+        }
+        ranges <- rbind(ranges, current_range)
+    }
+    if (na_ranges <- all(is.na(ranges))) {
+        maximal_range <- c(-1, 1)
+    }
+    else {
+        maximal_range <- c(min(ranges[, 1], na.rm = TRUE), max(ranges[, 2], na.rm = TRUE))
+    }
 
+    plot_stored <- list()
+    for (i in unique(df$id)) {
         df_subset <- df[df$id == i,]
         has_values <- !all(is.na(df_subset[df_subset$Series == what, "Value"]))
         annotations_list <- list(
-                text = paste('Session:', i),
-                xref = "paper",
-                yref = "paper",
-                yanchor = "bottom",
-                xanchor = "center",
-                align = "center",
-                x = 0.5,
-                y = 1,
+            text = paste('Session:', i),
+            xref = "paper",
+            yref = "paper",
+            yanchor = "bottom",
+            xanchor = "center",
+            align = "center",
+            x = 0.5,
+            y = 1,
             showarrow = FALSE)
+        axis_list <- list(zeroline = FALSE)
         if (has_values) {
             smoothed_model <- try(gam(Value ~ s(numericDate, bs = 'cs'), data = df_subset), silent = TRUE)
             smoothed_data <- predict(smoothed_model, newdata = df_subset)
-            a <- plot_ly(df_subset, x = ~ Index, y = ~ Value, hoverinfo='none', alpha = 0.1, color = I('black')) %>%
-                add_lines(showlegend = FALSE) %>%
-                add_lines(x = ~Index, y = smoothed_data, hoverinfo='text', text = ~paste(round(Value, 2), var_units),
+            a <- plot_ly(df_subset, x = ~ Index, y = ~ Value, hoverinfo = 'none',
+                         type = "scatter", mode = "lines",
+                         showlegend = FALSE, alpha = 0.1, color = I('black')) %>%
+                add_lines(x = ~ Index, y = smoothed_data, hoverinfo = 'text',
+                          text = ~ paste(round(Value, 2), var_units),
                           color = I('deepskyblue3'),
-                          showlegend = FALSE, alpha = 1) %>% layout(annotations = annotations_list)
+                          showlegend = FALSE, alpha = 1) %>%
+                layout(annotations = annotations_list,
+                       xaxis = axis_list, yaxis = c(axis_list, list(range = maximal_range * 1.02)))
         }
         else {
-            df_subset$Value <- 2
-            a <- plot_ly(df_subset, x = ~ Index, y = ~ Value, hoverinfo = 'none', alpha = 0, color = I('black'), type = "scatter", mode = "none", showlegend = FALSE) %>%
-                layout(annotations = annotations_list)
+            df_subset$Value <- if (na_ranges) 0 else mean(maximal_range)
+            a <- plot_ly(df_subset, x = ~ Index, y = ~ Value, hoverinfo = 'none',
+                         type = "scatter", mode = "none",
+                         showlegend = FALSE) %>%
+                layout(annotations = annotations_list,
+                       xaxis = axis_list, yaxis = c(axis_list, list(range = maximal_range * 1.02)))
         }
         plot_stored[[as.character(i)]] <- a
     }
 
-  y <- list(
-    title = var_name_units,
-    fixedrange = TRUE
-  )
-  x <- list(
-    title = 'Time',
-    fixedrange = TRUE
-  )
+    y <- list(title = var_name_units, fixedrange = TRUE)
+    x <- list(title = 'Time', fixedrange = TRUE)
 
-
-  return(subplot(plot_stored, nrows = 1, shareY = TRUE, margin = 0.002)  %>% config(displayModeBar = F) %>% layout(showlegend = FALSE, yaxis = y, xaxis = x, hovermode = 'closest'))
+    return(subplot(plot_stored, nrows = 1, shareY = FALSE, margin = 0.002)  %>%
+           config(displayModeBar = FALSE) %>%
+           layout(showlegend = FALSE, yaxis = y, xaxis = x, hovermode = 'closest'))
 
 
 }
