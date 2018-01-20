81fc2bafeed2ca6365cca9f642aef361d1c0f657
Ioannis Kosmidis <i.kosmidis@ucl.ac.uk>
2018-01-20 14:50:39 +0000

    About in ui

--- inst/shiny/ui.R
+++ inst/shiny/ui.R
@@ -19,7 +19,6 @@ ui <- shinydashboard::dashboardPage(
                           }
                          ")),
                          shinydashboard::sidebarMenu(
-                                             shinyjs::useShinyjs(),
                                              ## menuItem('', tabName = 'dashboard', icon = icon('dashboard')),
                                              ## hr(),
                                              ## fileInput('processed_data_path', 'Load processed data'),
@@ -49,13 +48,18 @@ ui <- shinydashboard::dashboardPage(
                                                  ## Out color: qualitative, even darker form choose_palette(gui = "shiny")
                                                  div(style="display: inline-block;vertical-align:top; width: 100px;", actionButton('plotButton', 'Plot', icon('area-chart'), style="color: #fff; background-color: #4FBF85; border-color: #00AB66", width = "80px")),
                                                  div(style="display: inline-block;vertical-align:top; width: 120px;", actionButton('changeUnits', 'Units', icon('balance-scale'), width = "80px"))),
-                                             div(style="display: inline-block;vertical-align:top; width: 100px;", actionButton('resetButton', 'Reset', icon('eraser'), style="color: #fff; background-color: #ED90A4; border-color: #E16A86", width = "80px")),
                                              hr(),
                                              div(class = 'form-group shiny-input-container', tags$label('Download data'),
                                                  div(class = 'input-group', downloadButton('download_data', 'Download procesed data'))),
                                              hr(),
-                                             div(class = "form-group shiny-input-container", tags$label("About"),                                                div(class = "input-group",  "The", a("trackeR", href = "https://cran.r-project.org/web/packages/trackeR/index.html"), "dashboard has been developed by Mr Robin Hornak and", a("Dr Ioannis Kosmidis", href="http://www.ucl.ac.uk/~ucakiko"))))
-                         ),
+                                             div(style="display: inline-block;vertical-align:top; width: 100px;", actionButton('resetButton', 'Reset', icon('eraser'), style="color: #fff; background-color: #ED90A4; border-color: #E16A86", width = "80px")),
+                                             hr()),
+                         div(class = "form-group shiny-input-container",
+                             p("Design and original development", br(), a("Robin Hornak"),  br(), a("Ioannis Kosmidis", href="http://www.ucl.ac.uk/~ucakiko")),
+                             p("Licence",
+                               a("GPL3", href = "https://www.gnu.org/licenses/gpl-3.0.en.html",
+                                 br(),
+                                 a("Bugs, issues, feature requests", href = "https://github.com/hfrick/trackeR/issues"))))),
                          shinydashboard::dashboardBody(
                                              fluidRow(
                                                  shinydashboard::box(
