7790dcc4b72371109ef144dbe35291d53d2f48b8
Ioannis Kosmidis <i.kosmidis@ucl.ac.uk>
2018-05-30 19:12:03 +0100

    got rid of cores argument; handled by user-specified backend

--- R/trackeRdata_zones.R
+++ R/trackeRdata_zones.R
@@ -28,7 +28,7 @@
 #' plot(runZones)
 #' @export
 zones <- function(object, session = NULL, what = c("speed", "heart.rate"), breaks = list(speed = 0:10,
-    heart.rate = c(0, seq(75, 225, by = 50), 250)), parallel = FALSE, cores = NULL, auto_breaks = TRUE,
+    heart.rate = c(0, seq(75, 225, by = 50), 250)), parallel = FALSE, auto_breaks = TRUE,
     n_zones = 9, ...) {
 
     ## select sessions
