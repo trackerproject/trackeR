099b3e2ee2aa975765019bcc10f04a6de3b00901
Ioannis Kosmidis <i.kosmidis@ucl.ac.uk>
2018-06-25 22:03:07 +0100

    Removed shiny option from read_directory

--- R/reading.R
+++ R/reading.R
@@ -574,9 +574,6 @@ read_container <- function(file, type = c("tcx", "gpx", "db3", "json"),
 #'     backend provided to \code{\link{foreach}}. Default is
 #'     \code{FALSE}.
 #' @param verbose Logical. Should progress reports be printed?
-#' @param shiny Logical. Should the output of \code{read_directory} be
-#'     made \code{\link[shiny]{reactive}}? For use in the shiny
-#'     interface. Default is \code{FALSE}.
 #' @inheritParams readX
 #' @inheritParams get_resting_periods
 #' @inheritParams impute_speeds
@@ -613,160 +610,125 @@ read_directory <- function(directory,
                           sport = NULL,
                           lgap = 30, lskip = 5, m = 11,
                           silent = FALSE,
-                          shiny = FALSE, ## only relevant for shiny interfaces
                           parallel = FALSE,
                           verbose = TRUE) {
 
-    read_expression <- quote({
+    tcxFiles <- list.files(directory, pattern = "tcx", ignore.case = TRUE, full.names = TRUE,
+                           no.. = TRUE)
+    gpxFiles <- list.files(directory, pattern = "gpx", ignore.case = TRUE, full.names = TRUE,
+                           no.. = TRUE)
+    db3Files <- list.files(directory, pattern = "db3", ignore.case = TRUE, full.names = TRUE,
+                           no.. = TRUE)
+    jsonFiles <- list.files(directory, pattern = "json", ignore.case = TRUE, full.names = TRUE,
+                            no.. = TRUE)
+    ltcx <- length(tcxFiles)
+    lgpx <- length(gpxFiles)
+    ldb3 <- length(db3Files)
+    ljson <- length(jsonFiles)
+    if ((ltcx == 0) & (ldb3 == 0) & (ljson == 0) & (lgpx == 0)) {
+        stop("The supplied directory contains no files with the supported formats.")
+    }
+    lall <- ltcx + lgpx + ldb3 + ljson
+    allFiles <- c(tcxFiles, gpxFiles, db3Files, jsonFiles)
+    fileType <- c(rep("tcx", ltcx), rep("gpx", lgpx), rep("db3", ldb3), rep("json", ljson))
 
-        tcxFiles <- list.files(directory, pattern = "tcx", ignore.case = TRUE, full.names = TRUE,
-                               no.. = TRUE)
-        gpxFiles <- list.files(directory, pattern = "gpx", ignore.case = TRUE, full.names = TRUE,
-                               no.. = TRUE)
-        db3Files <- list.files(directory, pattern = "db3", ignore.case = TRUE, full.names = TRUE,
-                               no.. = TRUE)
-        jsonFiles <- list.files(directory, pattern = "json", ignore.case = TRUE, full.names = TRUE,
-                                no.. = TRUE)
-        ltcx <- length(tcxFiles)
-        lgpx <- length(gpxFiles)
-        ldb3 <- length(db3Files)
-        ljson <- length(jsonFiles)
-        if ((ltcx == 0) & (ldb3 == 0) & (ljson == 0) & (lgpx == 0)) {
-            stop("The supplied directory contains no files with the supported formats.")
+    allData <- list()
+
+    if (aggregate) {
+        read_fun <- function(j) {
+            currentType <- fileType[j]
+            if (verbose) {
+                cat("Reading file", allFiles[j], paste0("(file ", j, " out of ", lall, ")"), "...\n")
+            }
+            try(do.call(what = paste0("read", toupper(currentType)),
+                        args = list(file = allFiles[j],
+                                    timezone = timezone,
+                                    speedunit = speedunit[[currentType]],
+                                    distanceunit = distanceunit[[currentType]])))
         }
-        lall <- ltcx + lgpx + ldb3 + ljson
-        allFiles <- c(tcxFiles, gpxFiles, db3Files, jsonFiles)
-        fileType <- c(rep("tcx", ltcx), rep("gpx", lgpx), rep("db3", ldb3), rep("json", ljson))
 
-        allData <- list()
-
-        if (aggregate) {
-            in_expression <- quote({
-
-                read_fun <- function(j) {
-                    currentType <- fileType[j]
-                    if (shiny) {
-                        incProgress(1/lall, detail = paste(j, "out of", lall,
-                                                           paste0("(", currentType, ")")))
-                    }
-                    if (verbose) {
-                        cat("Reading file", allFiles[j], paste0("(file ", j, " out of ", lall, ")"), "...\n")
-                    }
-                    try(do.call(what = paste0("read", toupper(currentType)),
-                                args = list(file = allFiles[j],
-                                            timezone = timezone,
-                                            speedunit = speedunit[[currentType]],
-                                            distanceunit = distanceunit[[currentType]])))
-                }
-
-                foreach_object <- eval(as.call(c(list(quote(foreach::foreach), j = seq.int(lall)))))
-                if (parallel) {
-                    setup_parallel()
-                    allData <- foreach::`%dopar%`(foreach_object, read_fun(j))
-                }
-                else {
-                    allData <- foreach::`%do%`(foreach_object, read_fun(j))
-                }
-
-
-            })
-            if (shiny) {
-                withProgress(expr = in_expression, message = 'Loading data', value = 0, quoted = TRUE)
-            }
-            else {
-                eval(in_expression)
-            }
-            if (verbose) {
-                cat("Cleaning up...")
-            }
-            sports <- sapply(allData, attr, which = "sport")
-            sport_to_use <- na.omit(sports)[1]
-            if (length(unique(sports)) > 1) {
-                warning(directory, "has files from multiple sports and aggregate = TRUE. Assumming that all files are ", sport_to_use)
-            }
-
-            allData <- do.call("rbind", allData[!sapply(allData, inherits, what = "try-error")])
-            from_distances <- if (is.null(from_distances)) TRUE else from_distances
-            allData <- trackeRdata(allData,
-                                   session_threshold = session_threshold,
-                                   correct_distances = correct_distances,
-                                   country = country,
-                                   mask = mask,
-                                   from_distances = from_distances,
-                                   sport = sport,
-                                   lgap = lgap,
-                                   lskip = lskip,
-                                   m = m,
-                                   silent = silent)
-            if (verbose) {
-                cat("Done\n")
-            }
+        foreach_object <- eval(as.call(c(list(quote(foreach::foreach), j = seq.int(lall)))))
+        if (parallel) {
+            setup_parallel()
+            allData <- foreach::`%dopar%`(foreach_object, read_fun(j))
         }
         else {
-            in_expression <- quote({
-
-                read_fun <- function(j) {
-                    currentType <- fileType[j]
-                    if (shiny) {
-                        incProgress(1/lall, detail = paste(j, "out of", lall,
-                                                           paste0("(", currentType, ")")))
-                    }
-                    if (verbose) {
-                        cat("Reading file", allFiles[j], paste0("(file ", j, " out of ", lall, ")"), "...\n")
-                    }
-                    try(read_container(file = allFiles[j],
-                                      type = currentType,
-                                      table = table,
-                                      timezone = timezone,
-                                      session_threshold = session_threshold,
-                                      correct_distances = correct_distances,
-                                      country = country,
-                                      mask = mask,
-                                      from_distances = from_distances,
-                                      speedunit = speedunit[[currentType]],
-                                      distanceunit = distanceunit[[currentType]],
-                                      sport = sport,
-                                      lgap = lgap,
-                                      lskip = lskip,
-                                      m = m,
-                                      silent = silent))
-                }
-
-                foreach_object <- eval(as.call(c(list(quote(foreach::foreach), j = seq.int(lall)))))
-                if (parallel) {
-                    setup_parallel()
-                    allData <- foreach::`%dopar%`(foreach_object, read_fun(j))
-                }
-                else {
-                    allData <- foreach::`%do%`(foreach_object, read_fun(j))
-                }
-            })
-            if (shiny) {
-                withProgress(expr = in_expression, message = 'Loading data', value = 0, quoted = TRUE)
-            }
-            else {
-                eval(in_expression)
-            }
-            if (verbose) {
-                cat("Cleaning up...")
-            }
-
-            allData <- do.call("c", allData[!sapply(allData, inherits, what = "try-error")])
-            if (verbose) {
-                cat("Done\n")
-            }
+            allData <- foreach::`%do%`(foreach_object, read_fun(j))
         }
 
-        ## clean and return
-        allData <- allData[!sapply(allData, is.null)]
-        allData
-    })
+        if (verbose) {
+            cat("Cleaning up...")
+        }
+        sports <- sapply(allData, attr, which = "sport")
+        sport_to_use <- na.omit(sports)[1]
+        if (length(unique(sports)) > 1) {
+            warning(directory, "has files from multiple sports and aggregate = TRUE. Assumming that all files are ", sport_to_use)
+        }
 
-    if (shiny) {
-        out <- reactive(read_expression, quoted = TRUE)
-        return(out())
+        allData <- do.call("rbind", allData[!sapply(allData, inherits, what = "try-error")])
+        from_distances <- if (is.null(from_distances)) TRUE else from_distances
+        allData <- trackeRdata(allData,
+                               session_threshold = session_threshold,
+                               correct_distances = correct_distances,
+                               country = country,
+                               mask = mask,
+                               from_distances = from_distances,
+                               sport = sport,
+                               lgap = lgap,
+                               lskip = lskip,
+                               m = m,
+                               silent = silent)
+        if (verbose) {
+            cat("Done\n")
+        }
     }
     else {
-        eval(read_expression)
+        read_fun <- function(j) {
+            currentType <- fileType[j]
+            if (verbose) {
+                cat("Reading file", allFiles[j], paste0("(file ", j, " out of ", lall, ")"), "...\n")
+            }
+            try(read_container(file = allFiles[j],
+                               type = currentType,
+                               table = table,
+                               timezone = timezone,
+                               session_threshold = session_threshold,
+                               correct_distances = correct_distances,
+                               country = country,
+                               mask = mask,
+                               from_distances = from_distances,
+                               speedunit = speedunit[[currentType]],
+                               distanceunit = distanceunit[[currentType]],
+                               sport = sport,
+                               lgap = lgap,
+                               lskip = lskip,
+                               m = m,
+                               silent = silent))
+        }
+
+        foreach_object <- eval(as.call(c(list(quote(foreach::foreach), j = seq.int(lall)))))
+        if (parallel) {
+            setup_parallel()
+            allData <- foreach::`%dopar%`(foreach_object, read_fun(j))
+        }
+        else {
+            allData <- foreach::`%do%`(foreach_object, read_fun(j))
+        }
+
+        if (verbose) {
+            cat("Cleaning up...")
+        }
+
+        allData <- do.call("c", allData[!sapply(allData, inherits, what = "try-error")])
+        if (verbose) {
+            cat("Done\n")
+        }
     }
+
+    ## clean and return
+    allData <- allData[!sapply(allData, is.null)]
+    if (aggregate) {
+        attr(allData, "file") <- rep(NA, length(allData))
+    }
+    allData
 }
