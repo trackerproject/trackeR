75ec8adc63855223c4249508ace4cb8cada7aa2a
Ioannis Kosmidis <i.kosmidis@ucl.ac.uk>
2018-09-12 15:14:03 +0100

    Do not run examples for concentration_profile

--- R/concentrationProfile.R
+++ R/concentrationProfile.R
@@ -102,12 +102,14 @@ concentration_profile.distrProfile <- function(object,
 #'     \code{\link{smoother_control.distrProfile}}.
 #'
 #' @examples
+#' \dontrun{
 #' data('runs', package = 'trackeR')
 #' dProfile <- distributionProfile(runs, session = 1:3, what = 'speed',
 #'                                 grid = seq(0, 12.5, by = 0.05))
 #' cProfile <- concentrationProfile(dProfile)
 #' plot(cProfile, smooth = FALSE)
 #' plot(cProfile)
+#' }
 #' @export
 plot.conProfile <- function(x,
                             session = NULL,
